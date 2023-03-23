ffbb38d39095719acc6eec9cc565363f93d34b3f
adrian-siv <adrian.ssiv@gmail.com>
2023-03-23 20:48:36 -0300

    refactor: delete serverless framework default return message

--- index.js
+++ index.js
@@ -54,7 +54,6 @@ module.exports.handler = async () => {
       statusCode: 200,
       body: JSON.stringify(
         {
-          message: "Go Serverless v3.0! Your function executed successfully!",
           productsList
         },
         null,

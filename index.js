c01e60c5171c938f4187b6ebd9b7e8309469bbf2
adrian-siv <adrian.ssiv@gmail.com>
2023-03-23 02:43:53 -0300

    feat: add top products web scraper function

--- index.js
+++ index.js
@@ -1,13 +1,67 @@
-module.exports.handler = async (event) => {
+const chromium = require("@sparticuz/chromium");
+const puppeteer = require("puppeteer-core");
+
+module.exports.handler = async () => {
+  const productsList = [];
+
+  const browser = await puppeteer.launch({
+    // headless: false,
+    // defaultViewport: false,
+    // args: ['--start-maximized']
+    args: chromium.args,
+    defaultViewport: chromium.defaultViewport,
+    executablePath: await chromium.executablePath(),
+    headless: chromium.headless
+  });
+  const page = await browser.newPage();
+  await page.goto("https://www.amazon.com.br/bestsellers");
+
+  await page.setViewport({ width: 1366, height: 768 });
+
+  const productHandles = await page.$$(
+    "#anonCarousel1 .a-carousel > .a-carousel-card"
+  );
+
+  for (const producthandle of productHandles.slice(0, 3)) {
+    const title = await page.evaluate(
+      (el) =>
+        el.querySelector(".p13n-sc-truncate-desktop-type2.p13n-sc-truncated")
+          .textContent,
+      producthandle
+    );
+    const price = await page.evaluate(
+      (el) => el.querySelector("._cDEzb_p13n-sc-price_3mJ9Z").textContent,
+      producthandle
+    );
+    const img = await page.evaluate(
+      (el) =>
+        el
+          .querySelector(
+            ".a-dynamic-image.p13n-sc-dynamic-image.p13n-product-image"
+          )
+          .getAttribute("src"),
+      producthandle
+    );
+
+    productsList.push({
+      title,
+      price,
+      img
+    })
+  }
+
+  await browser.close();
+
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
         message: "Go Serverless v3.0! Your function executed successfully!",
-        input: event,
+        productsList
       },
       null,
       2
     ),
   };
+
 };
